75524f99c62f2f192d7443b96f2a9c9703075826
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-04-02 12:54:57 -0500

    Added scrape and manage folder

---
+++ scrape.py
@@ -0,0 +1,33 @@
+import requests
+import time
+from datetime import datetime
+import os
+
+def convertEpoch(timestamp):
+    epoch_start = datetime(1970,1,1)
+    epoch = (timestamp - epoch_start).total_seconds()
+    return epoch
+
+def curls(timestamp):
+    req = 'https://na.api.pvp.net/api/lol/na/v4.1/game/ids?beginDate=' + str(timestamp) + '&api_key=1c692a41-8f5a-4360-bc7e-5c86dd323a91'
+    r = requests.get(req)
+    return r
+
+t = datetime.now() # Get the time right now
+t = t.replace(second = 0, microsecond = 0)
+if (t.minute % 5 != 0):
+    newminute = t.minute - (t.minute % 5)
+    t = t.replace(minute=newminute)
+
+epoch = convertEpoch(t)
+first = 1
+for i in range(0, 96):
+    epoch -= 300
+    req = curls(int(epoch))
+    time.sleep(1)
+    if first is 1:
+        os.mkdir(t.strftime('matches/%Y-%m-%d_%H-%M'))
+        first = 0
+    txt_file = open(('matches/' + t.strftime('%Y-%m-%d_%H-%M') + '/' + str(int(epoch)) + '.json'), 'w')
+    txt_file.write(str(req.json()))
+
