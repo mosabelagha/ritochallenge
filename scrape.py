5096a4aca05cf85560491f9731a6f2afd0d33987
Mosab Elagha <mosabelagha@gmail.com>
2015-04-04 15:53:54 -0500

    Added files - Automator

--- scrape.py
+++ scrape.py
@@ -3,6 +3,8 @@ import time
 from datetime import datetime
 import os
 
+requests.packages.urllib3.disable_warnings()
+
 def convertEpoch(timestamp):
     epoch_start = datetime(1970,1,1)
     epoch = (timestamp - epoch_start).total_seconds()
@@ -10,7 +12,7 @@ def convertEpoch(timestamp):
 
 def curls(timestamp):
     req = 'https://na.api.pvp.net/api/lol/na/v4.1/game/ids?beginDate=' + str(timestamp) + '&api_key=1c692a41-8f5a-4360-bc7e-5c86dd323a91'
-    r = requests.get(req, verify = False)
+    r = requests.get(req, verify=False)
     return r
 
 t = datetime.now() # Get the time right now
@@ -30,4 +32,5 @@ for i in range(0, 96):
         first = 0
     txt_file = open(('matches/' + t.strftime('%Y-%m-%d_%H-%M') + '/' + str(int(epoch)) + '.json'), 'w')
     txt_file.write(str(req.json()))
+    print(str(i+1) + "/96 files retrieved")
 
