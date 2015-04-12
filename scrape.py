1232fd1cd1a8744b42dad08b2ec6a9857bfb18a9
Jeffrey Huang <jeffrey.huang977@gmail.com>
2015-04-12 15:44:04 -0500

    Added match grabbing and shit

--- scrape.py
+++ scrape.py
@@ -15,6 +15,11 @@ def curls(timestamp):
     r = requests.get(req, verify=False)
     return r
 
+def match_req(matchid):
+    req = 'https://na.api.pvp.net/api/lol/na/v2.2/match/' + str(matchid) + '?includeTimeline=1&api_key=1c692a41-8f5a-4360-bc7e-5c86dd323a91'
+    r = requests.get(req, verify=False)
+    return r
+
 t = datetime.now() # Get the time right now
 t = t.replace(second = 0, microsecond = 0)
 if (t.minute % 5 != 0):
@@ -23,11 +28,24 @@ if (t.minute % 5 != 0):
 
 epoch = convertEpoch(t)
 os.mkdir(t.strftime('matches/%Y-%m-%d_%H-%M'))
+fuckyoumosab = []
 for i in range(0, 96):
     epoch -= 300
     req = curls(int(epoch))
+    fuckyoumosab.append(req.json())
     time.sleep(1)
     txt_file = open(('matches/' + t.strftime('%Y-%m-%d_%H-%M') + '/' + str(int(epoch)) + '.json'), 'w')
     txt_file.write(str(req.json()))
     print(str(i+1) + "/96 files retrieved")
+
+for row in fuckyoumosab:
+    i = 0
+    for matchid in row:
+        req = match_req(matchid)
+        time.sleep(1)
+        txt_file = open(('gameinfo/' + str(matchid) + '.json'), 'w')
+        txt_file.write(str(req.json()).replace("u'", "'").replace("False", "false").replace("True", "true"))
+        print(str(i+1) + "/" + str(len(row)) + " files retrieved")
+        i += 1
+
 
